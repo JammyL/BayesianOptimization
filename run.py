462b0e8ac2733af04f083fc4bb4adf07e6baffc7
JammyL <Jamie-Leppard@outlook.com>
2021-02-11 18:37:30 +0000

    Edits to run.py for multiple qubits

--- run.py
+++ run.py
@@ -1,22 +1,33 @@
 from problems import threeQubitCircuit
+from qutip.tensor import tensor
 import qutip as qt
 import numpy as np
 import pickle
 import sys
 
 args = sys.argv[1:]
-if len(args) < 2:
+if len(args) < 3:
     configPath = './configs/three_q/bad_path/config_1.yaml'
     outputPath = 'results_10.pickle'
+    numberStates = 1
 else:
     configPath = args[0]
     outputPath = args[1]
+    numberStates = int(args[2])
+
+initial_state_list = []
+for i in range(numberStates):
+    spins = []
+    bits = [int(x) for x in '{:03b}'.format(i)]
+    for b in bits:
+        spins.append(qt.basis(2,b))
+    initial_state_list.append(tensor(spins))
 
 controlResults = []
 transferResults = []
 
 for _ in range(1000):
-    p = threeQubitCircuit(configPath=configPath)
+    p = threeQubitCircuit(initialState_list=initial_state_list, configPath=configPath,)
     p.default_opt()
     tResult, tCost, cResult, cCost = p.get_result()
     if p.ControlOptimizer != None:
